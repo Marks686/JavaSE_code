ac046ab5a44ee0d8d5edc487889f9aa0c400b6db
Marks686 <17829791810@163.com>
2022-07-25 02:42:14 +0800

    我尝试第二次使用idea提交idea，新增了部分的代码

--- J-20220725-First acquaintance with java/src/HelloWorld.java
+++ J-20220725-First acquaintance with java/src/HelloWorld.java
@@ -1,3 +1,11 @@
 public class HelloWorld {
+    //main -> psvm
+    public static void main(String[] args) {
 
+        //sout
+        System.out.println("hello");
+
+        // 10.sout
+        System.out.println(10);
+    }
 }
