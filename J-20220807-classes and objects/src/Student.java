d4b6df8cfd13b15bca4f8046f7a97382303a8e15
Marks686 <17829791810@163.com>
2022-08-09 20:34:53 +0800

    提交方法和类的代码

---
+++ J-20220807-classes and objects/src/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    public String name;
+    public int age;
+
+
+    Student(){
+        System.out.println("不带参数的构造方法");
+    }
+
+    Student(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    public void setStudent(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    public void print(){
+        System.out.println(this.name+" => "+this.age );
+    }
+
+    public static void main(String[] args) {
+         Student student = new Student();
+         student.setStudent("bit",12);
+         student.print();
+    }
+}
