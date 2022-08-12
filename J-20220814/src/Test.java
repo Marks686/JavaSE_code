bb777dde61b2234b6d7da90530185251ff32f735
Marks686 <17829791810@163.com>
2022-08-12 23:44:42 +0800

    提交方法和类的代码

---
+++ J-20220814/src/Test.java
@@ -0,0 +1,15 @@
+class Person{
+    public int age;
+    public String name;
+
+
+    //行为
+    public void eat(){
+        System.out.println("吃饭！");
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
